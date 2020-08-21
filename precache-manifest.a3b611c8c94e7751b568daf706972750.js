c3c4a467a680cf68200d74dbbf162c53d15f1841
marianoGnlz <marianoGnlz@users.noreply.github.com>
2020-08-21 19:06:18 -0300

    Updates

---
+++ precache-manifest.a3b611c8c94e7751b568daf706972750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac52c107cf421251c513b0883f44627",
+    "url": "/buscadordenoticias/index.html"
+  },
+  {
+    "revision": "c1acf20d42fc1b8fc6f3",
+    "url": "/buscadordenoticias/static/css/main.9dc4bdeb.chunk.css"
+  },
+  {
+    "revision": "1098b8e36d57a18258ab",
+    "url": "/buscadordenoticias/static/js/2.1fe8522b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buscadordenoticias/static/js/2.1fe8522b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1acf20d42fc1b8fc6f3",
+    "url": "/buscadordenoticias/static/js/main.ad93dd3c.chunk.js"
+  },
+  {
+    "revision": "3a69808b6aebfee44621",
+    "url": "/buscadordenoticias/static/js/runtime-main.413142d5.js"
+  }
+]);
