f75813916d2b3e9f4fbfa28b1d70f0ed1ce3ae86
marianoGnlz <marianoGnlz@users.noreply.github.com>
2020-08-21 19:03:30 -0300

    Updates

---
+++ precache-manifest.91d3f8af509b0b0e77ebdc309094da8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877b18edd7882fece30ffc1b61d4bb15",
+    "url": "/buscadorDeNoticias/index.html"
+  },
+  {
+    "revision": "e15037aa56335aba4a4a",
+    "url": "/buscadorDeNoticias/static/css/main.9dc4bdeb.chunk.css"
+  },
+  {
+    "revision": "1098b8e36d57a18258ab",
+    "url": "/buscadorDeNoticias/static/js/2.1fe8522b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buscadorDeNoticias/static/js/2.1fe8522b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15037aa56335aba4a4a",
+    "url": "/buscadorDeNoticias/static/js/main.d1c96734.chunk.js"
+  },
+  {
+    "revision": "80418f10644d74c93bef",
+    "url": "/buscadorDeNoticias/static/js/runtime-main.c0ed44ca.js"
+  }
+]);
